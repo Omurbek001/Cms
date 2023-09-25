55b36fbcdad1f503f43ae0fd590be069347ae59b
shagent007 <sagent118@gmail.com>
2023-09-25 08:44:59 +0600

    configuration

--- Cms.Setup/Program.cs
+++ Cms.Setup/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Reflection;
+using Cms.ECommerce;
+using Cms.EducationPortal;
+using Cms.Shared.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true)
+    .Build();
+
+var eCommerce = ECommerce.GetECommerceAssembly();
+var education = EducationPortal.GetEducationPortalAssembly(); 
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddLogging();
+serviceCollection.AddServices(configuration, new List<Assembly>{eCommerce, education});
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+var dataContext = serviceProvider.GetRequiredService<DataContext>();
+
+var roles = new List<IdentityRole>()
+{
+    new()
+    {
+        Name = "Administrator",
+        ConcurrencyStamp = Guid.NewGuid().ToString()
+    }
+};
+
+foreach (var role in roles)
+{
+    await roleManager.CreateAsync(role);
+}
+
+await dataContext.SaveChangesAsync();
